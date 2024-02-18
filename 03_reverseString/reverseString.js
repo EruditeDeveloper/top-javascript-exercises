23a652fa4273e4905ac55332b4b7672d2c79cce3
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-18 09:15:14 +0000

    Continuity is great - I am onexercise  03. remove from array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let reversedStringArray = [];
+	for (let i = 0; i < string.length; i++) {
+		reversedStringArray.unshift(string[i]);
+	}
+	return (reversedStringArray.join(""));
 };
 
 // Do not edit below this line
