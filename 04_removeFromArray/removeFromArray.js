ad5cde1898d3e3400a135b4fd2cccc1ee00b1f9f
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-18 11:00:14 +0000

    successfully completed excercise 05 now

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function(array, ...args) {
-    
+const removeFromArray = function (array, ...args) {
+	for (let i = 0; i < args.length; i++) {
+		let argToRemove = args[i];
+		for (let d = 0; d < array.length; d++) {
+			let arrItem = array[d];
+			if (argToRemove === arrItem) {
+				let removedElements = array.splice(d, 1);
+				console.log(removedElements);
+				d = d - 1;
+			}
+		}
+	}
+
+	return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	let greaterNum;
+	let smallerNum;
+	let inBetweenArray = [];
+    if ((num1 < 0 || num2 < 0) || (typeof num1 !== 'number' || typeof num2 !== 'number')) {
+        return "ERROR"
+    }
+	if (num1 > num2) {
+		greaterNum = num1;
+		smallerNum = num2;
+	} else {
+		greaterNum = num2;
+		smallerNum = num1;
+	}
+	for (let i = smallerNum; i <= greaterNum; i++) {
+        inBetweenArray.push(i)
+    }
+    const sum = inBetweenArray.reduce((accumulator, CurrentValue) => accumulator + CurrentValue, 0)
+    return sum
 
 };
 
