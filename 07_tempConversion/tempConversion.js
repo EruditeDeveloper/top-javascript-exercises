8487bc9e48b7197f7a3aa6e0a34f4b444a2c204d
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-18 12:35:34 +0000

    successfully completed excercise 06

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+	let temp = temperature;
+	let convertedTemp = ((temp - 32) * 5) / 9;
+    let roundedConvertedTemp = Math.round(convertedTemp * 10) /10
+	return roundedConvertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+	let temp = temperature;
+	let convertedTemp = (temp * 9) / 5 + 32;
+    let roundedConvertedTemp = Math.round(convertedTemp * 10) /10
+
+	return roundedConvertedTemp;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
