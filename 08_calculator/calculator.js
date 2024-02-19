a55712a8f66c1c4f9ad171ca037d3c57617a4e5f
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-19 11:48:24 +0000

    successfully completed excercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,56 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	let sum = 0;
+	array.forEach((element) => {
+		if (typeof element === "number") {
+			sum = sum + element;
+		}
+	});
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function (numArr) {
+	let total;
 
+	for (let i = 0; i < numArr.length; i++) {
+		if (total) {
+			total *= numArr[i];
+		} else {
+			total = numArr[i];
+		}
+	}
+
+	return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let numberArray = [];
+	for (let i = num; i > 0; i--) {
+		numberArray.push(i);
+	}
+
+	let number = numberArray.reduce((acc, value) => acc * value, 1);
+
+	return number;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
