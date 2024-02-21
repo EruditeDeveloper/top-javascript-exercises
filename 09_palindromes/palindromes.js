ebc092f5fe0639b01ed5e166b7c6c0f7e4d820f5
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-21 17:14:18 +0000

    working on exercise 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let result;
+	for (let i; i < string.length; i++) {
+		let char = string[i];
+        char.toLower()
+        if ()
+	}
 
+	if (string.length % 2 !== 0) {
+		let parts = string.split("", 2);
+		let firstHalf = parts[0];
+		let secondHalf = parts[1];
+	} else {
+		let middle = Math.floor(string.length / 2);
+		let firstPart = string.substring(0, middle);
+		let secondPart = string.substring(middle);
+	}
 };
 
 // Do not edit below this line
