f9bce459a0228753685aa3e7db8f2dd31a92a776
Erudite <120523951+EruditeDeveloper@users.noreply.github.com>
2024-02-22 19:38:45 +0000

    Palindromes - I have a huge bunch of redundant code - just normalise each string remove all non letter characters, then lower case, then check the string is the same as its reversed self then it should be a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,76 @@
 const palindromes = function (string) {
-	let result;
-	for (let i; i < string.length; i++) {
+	// 1. remove everything except letters then make lowercase
+
+	let pureString = "";
+
+	let firstHalf,
+		secondHalf,
+		middleChar = "";
+
+	for (let i = 0; i < string.length; i++) {
 		let char = string[i];
-        char.toLower()
-        if ()
+		if (
+			(char.charCodeAt() >= 65 && char.charCodeAt() <= 90) ||
+			(char.charCodeAt() >= 97 && char.charCodeAt() <= 122)
+		) {
+			console.log("if true it means it is indeed a letter");
+			pureString += char;
+		}
 	}
 
-	if (string.length % 2 !== 0) {
-		let parts = string.split("", 2);
-		let firstHalf = parts[0];
-		let secondHalf = parts[1];
+	// make sure to remove all numbers from the final pureString before moving forward.
+
+	let pureStringWithoutNumbers = "";
+	for (let i = 0; i < pureString.length; i++) {
+		if (isNaN(Number(pureString[i]))){
+			pureStringWithoutNumbers+= pureString[i];
+		}
+	}
+	
+	pureString = pureStringWithoutNumbers;
+
+	let length = pureString.length;
+	pureString = pureString.toLowerCase();
+
+	// 2. split string at middle index for odd beginning part, middle index, last part. [0][x][-1]
+	// 3. for even split string in 2 parts
+
+	if (length % 2 === 0) {
+		firstHalf = pureString.substring(0, length / 2);
+		secondHalf = pureString.substring(length / 2);
 	} else {
-		let middle = Math.floor(string.length / 2);
-		let firstPart = string.substring(0, middle);
-		let secondPart = string.substring(middle);
+		let middleIndex = Math.floor(length / 2);
+		middleChar = pureString[middleIndex];
+		firstHalf = pureString.slice(0, middleIndex);
+		secondHalf = pureString.slice(middleIndex + 1);
 	}
-};
 
+	// 4. reverse one of them then use one loop to iterate and test each one. to see if they are the exact same char
+
+	let reversedStringArray = [];
+	for (let i = 0; i < secondHalf.length; i++) {
+		reversedStringArray.unshift(secondHalf[i]);
+	}
+
+	let reversedSecondHalf = reversedStringArray.join("");
+
+	isEachCharacterTheSameArray = [];
+	if (firstHalf.length === reversedSecondHalf.length) {
+		for (let i = 0; i < reversedSecondHalf.length; i++) {
+			if (firstHalf[i] === reversedSecondHalf[i]) {
+				isEachCharacterTheSameArray.push(true);
+			} else {
+				isEachCharacterTheSameArray.push(false);
+			}
+		}
+	}
+	for (let i = 0; i < isEachCharacterTheSameArray.length; i++) {
+		if (isEachCharacterTheSameArray[i] === false) {
+			return false;
+		}
+	}
+
+	return true;
+};
 // Do not edit below this line
 module.exports = palindromes;
